6ca5b7198591cc906803978e289fd64da0cc3c37
Khang Hoang <hoangtrieukhang@gmail.com>
2016-01-02 23:41:08 +0700

    webpack + react

---
+++ app/components/ArticleListItem.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ArticleListItem extends Component {
+    render() {
+        return (
+            <li>hello world</li>
+        )
+    }
+}
+
+export default ArticleListItem;
