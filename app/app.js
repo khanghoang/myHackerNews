3c1106f41bf00c721e012f97c45a5466cd02931c
Khang Hoang <hoangtrieukhang@gmail.com>
2016-01-03 00:07:14 +0700

    Webpack + hot loader + material design

---
+++ app/app.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import ArticleListItem from './components/ArticleListItem';
+
+ReactDOM.render(
+    <ArticleListItem />,
+    document.getElementById('container')
+);
--- app/components/ArticleListItem.js
+++ app/components/ArticleListItem.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
+import ListItem from 'material-ui/lib/lists/list-item';
 
 class ArticleListItem extends Component {
     render() {
         return (
-            <li>hello world 3</li>
+            <ListItem
+                primaryText="Hello world"
+                secondarytext={
+                    <p> hello there </p>
+                }
+            />
         )
     }
 }
